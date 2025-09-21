4485b02459b299bf2408033101d27250a3bcab3e
Alyona R <99629970+AlenaRogojina@users.noreply.github.com>
2025-09-21 08:58:44 -0500

    Added comments to signify blocks

--- WIP.py
+++ WIP.py
@@ -1,1 +1,17 @@
-print("This is a work in progress")
+# Grab input color (for 1)
+
+# Generate random color (for 2)
+
+# Generate new colors based on input color
+
+# Radio buttons for complementary, analogous, triadic
+
+# Output color blocks
+
+# Output template preview
+
+# Potentially convert PDF to PPT
+
+# Output template itself
+
+# Allow download template
